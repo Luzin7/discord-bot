3d68f3e763eb93f99b6810f6c8f1cfb68d2ca1d4
Luzin7 <lvictor.dutra@gmail.com>
2023-04-16 19:18:57 -0300

    refact(index.js): importa funções essenciais e retira bagunça

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
-const { REST, Routes } = require('discord.js');
-
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { DISCORD_TOKEN, CLIENT_ID } = process.env;
+const { DISCORD_TOKEN } = process.env;
 
-const commands = [
-	{
-		name: 'ping',
-		description: 'Responde com Pong!',
-	},
-];
-
-const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
-
-(async () => {
-	try {
-		console.log('Atualizando e adicionado comando slash');
-
-		await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-		console.log('Comando slash adicionado e atualizado');
-	} catch (error) {
-		console.error(error);
-	}
-})();
-
-const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-	console.log(`Rufando o bot ${client.user.tag}!`);
-});
+client.commands = new Collection();
 
-client.on('interactionCreate', async (interaction) => {
+const foldersPath = path.join(__dirname, 'commands');
+const foldersInCommandsDir = fs.readdirSync(foldersPath);
+
+for (const folder of foldersInCommandsDir) {
+	const commandsPath = path.join(foldersPath, folder);
+	const filesInCommandsDir = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith('.js'));
+
+	for (const file of filesInCommandsDir) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+
+		'data' in command && 'execute' in command
+			? client.commands.set(command.data.name, command)
+			: console.log(
+					`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+			  );
+	}
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
 
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(
+			`O comando ${interaction.commandName} provavelmente não existe.`,
+		);
+		return;
 	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				ephemeral: true,
+			});
+		} else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				ephemeral: true,
+			});
+		}
+	}
+});
+
+client.once(Events.ClientReady, (c) => {
+	console.log(`Runfando ${c.user.tag}`);
 });
 
 client.login(DISCORD_TOKEN);
